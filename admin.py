e55476e4d33522e8f9bdf937a6e9e841094e2047
parag jain <paragikjain786@gmail.com>
2025-05-17 17:17:45 +0530

    admin panle table view

--- admin.py
+++ admin.py
@@ -1,7 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from db import ResponsesColl
+
 
 admin_router = APIRouter()
+templates = Jinja2Templates(directory="templates/admin")
 
-@admin_router.get("/admin/dashboard")
-def get_dashboard():
-    return {"message": "Welcome to the Admin Dashboard"}
+@admin_router.get("/admin/dashboard", response_class=HTMLResponse)
+def get_dashboard(request: Request):
+    # Fetch all documents from responses collection
+    response_docs = list(ResponsesColl.find({}))
+
+    # Convert ObjectId to string for JSON
+    for doc in response_docs:
+        doc["_id"] = str(doc["_id"])
+
+    return templates.TemplateResponse("admin_table.html", {
+        "request": request,
+        "data": response_docs
+    })
--- readme.md
+++ readme.md
@@ -3,4 +3,4 @@
 * First set MONGO_URI in your env
 `$env:MONGO_URI = "mongodb+srv://<username>:<password>@questionform.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000"`
 * Run below command to start server
-`uvicorn main:app --reload`
+`uvicorn main:app --reload --reload-dir`
